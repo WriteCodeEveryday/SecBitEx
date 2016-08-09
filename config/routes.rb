9134bf30e2b4934b3a0f495720327e42ef844b2f
Lazaro Herrera <lazherrera@gmail.com>
2016-08-09 04:08:22 -0400

    Stats Page And Some More Tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/features/custody' => 'visitors#custody'
   get '/features/peertopeer' => 'visitors#peertopeer'
   get '/features/opensource' => 'visitors#opensource'
+  get '/stats' => 'visitors#stats'
   devise_for :users
   resources :users
 end
